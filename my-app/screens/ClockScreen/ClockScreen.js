e24b726a1f48012c2af659cb1ab2ee0eabda6a7c
lulemar16 <128646816+lulemar16@users.noreply.github.com>
2024-05-06 12:35:54 +0300

    auto remove deadlines

--- my-app/screens/ClockScreen/ClockScreen.js
+++ my-app/screens/ClockScreen/ClockScreen.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { View, ScrollView } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, ScrollView, Button } from 'react-native';
 import AlarmsSection from './AlarmsScreen';
 import TimersSection from './TimersScreen';
 import DeadlinesSection from './DeadlinesScreen';
@@ -11,11 +11,21 @@ import dayjs from 'dayjs';
 import { getFirestore, doc, setDoc, getDoc, collection, getDocs, addDoc, deleteDoc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import {useNavigation} from "@react-navigation/native";
-import PushNotification from 'react-native-push-notification';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 
 const auth = getAuth();
 const db = getFirestore();
 const userUID = auth.currentUser ? auth.currentUser.uid : "";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 const ClockScreen = () => {
   const [alarms, setAlarms] = useState([]);
@@ -32,6 +42,10 @@ const ClockScreen = () => {
   const [selectedAlarmTime, setSelectedAlarmTime] = useState(new Date());
   const [selectedTimerTime, setSelectedTimerTime] = useState(new Date());
   const [selectedDeadlineTime, setSelectedDeadlineTime] = useState(new Date());
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
   const userRef = doc(db, 'users', userUID);
   const alarmsRef = collection(userRef, 'alarms');
@@ -49,24 +63,103 @@ const ClockScreen = () => {
   // }, [timers]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDataAndRemovePassedDeadlines = async () => {
+      // Fetch data for alarms, timers, and deadlines
       const alarmsSnapshot = await getDocs(alarmsRef);
       const alarmsData = alarmsSnapshot.docs.map(doc => doc.data());
       const timersSnapshot = await getDocs(timersRef);
       const timersData = timersSnapshot.docs.map(doc => doc.data());
       const deadlinesSnapshot = await getDocs(deadlinesRef);
       const deadlinesData = deadlinesSnapshot.docs.map(doc => doc.data());
+      
+      // Set the state for alarms, timers, and deadlines
       setAlarms(alarmsData);
       setTimers(timersData);
       setDeadlines(deadlinesData);
+  
+      // Remove passed deadlines
+      const now = dayjs();
+      const updatedDeadlines = [];
+      for (const deadline of deadlinesData) {
+        const jsDate = new Date(deadline.time.seconds * 1000);
+        const deadlineTime = dayjs(jsDate);
+        if (now.isBefore(deadlineTime)) {
+          updatedDeadlines.push(deadline);
+        }
+      }
+      setDeadlines(updatedDeadlines);
     };
-    PushNotification.configure();
-    PushNotification.requestPermissions();
-    fetchData();
+  
+    fetchDataAndRemovePassedDeadlines();
+  
+    const interval = setInterval(fetchDataAndRemovePassedDeadlines, 60000);
+    return () => clearInterval(interval);
+  }, []);  
 
-    const timerInterval = setInterval(updateTimers, 1000);
-    return () => clearInterval(timerInterval);
-  }, [timers]);
+
+  // useEffect(() => {
+  //   registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+  //   notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+  //     setNotification(notification);
+  //   });
+
+  //   responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+  //     // console.log(response);
+  //   });
+
+  //   return () => {
+  //     Notifications.removeNotificationSubscription(notificationListener.current);
+  //     Notifications.removeNotificationSubscription(responseListener.current);
+  //   };
+  // }, []);
+
+  async function schedulePushNotification() {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: 'Here is the notification body',
+        data: { data: 'goes here' },
+      },
+      trigger: { seconds: 2 },
+    });
+  }
+  
+  async function registerForPushNotificationsAsync() {
+    let token;
+  
+    if (Platform.OS === 'android') {
+      await Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      // Learn more about projectId:
+      // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+      token = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig.extra.eas.projectId,
+      });
+      // console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    return token;
+  }
 
 
   const addAlarm = async (title, time) => {
@@ -148,9 +241,9 @@ const ClockScreen = () => {
   };
 
   const updateTimers = async () => {
-    console.log("Updating timers...");
+    // console.log("Updating timers...");
     const updatedTimers = timers.map(async (timer, index) => {
-      console.log(`Timer ${index + 1}: ${timer.time}`);
+      // console.log(`Timer ${index + 1}: ${timer.time}`);
       if (!timer.paused) {
         const [hours, minutes, seconds] = timer.time.split(':').map(Number);
         let totalSeconds = hours * 3600 + minutes * 60 + seconds;
@@ -159,7 +252,7 @@ const ClockScreen = () => {
         if (totalSeconds === 0) {
           const initialTime = timer.initialTime;
           await toggleTimer(index);
-          console.log(`Timer ${index + 1} reached 0 seconds. Resetting to initial time: ${initialTime}`);
+          // console.log(`Timer ${index + 1} reached 0 seconds. Resetting to initial time: ${initialTime}`);
           return { ...timer, time: initialTime };
         }
   
@@ -167,14 +260,14 @@ const ClockScreen = () => {
           .startOf('day')
           .add(totalSeconds, 'seconds')
           .format('HH:mm:ss');
-        console.log(`Timer ${index + 1} updated to: ${newTime}`);
+        // console.log(`Timer ${index + 1} updated to: ${newTime}`);
         return { ...timer, time: newTime };
       }
       return timer;
     });
   
     const updatedTimersData = await Promise.all(updatedTimers);
-    console.log("Updated timers:", updatedTimersData);
+    // console.log("Updated timers:", updatedTimersData);
     setTimers(updatedTimersData);
   };  
   
@@ -240,8 +333,41 @@ const ClockScreen = () => {
     setDeadlines(updatedDeadlines);
   };
 
+  const removeDeadlineById = async (id) => {
+    const deadlineDocRef = doc(deadlinesRef, id);
+    await deleteDoc(deadlineDocRef);
+    const updatedDeadlines = deadlines.filter((deadline) => deadline.id !== id);
+    setDeadlines(updatedDeadlines);
+  };
+
+  const sendPushNotification = async (expoPushToken) => {
+    const message = {
+      to: expoPushToken,
+      sound: 'default',
+      title: 'My Notification Title',
+      body: 'This is the body of the notification',
+      data: { data: 'goes here' },
+    };
+  
+    await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Accept-encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
+  };
+
   return (
       <ScrollView style={styles.container}>
+        <Button
+          title="Press to Send Notification"
+          onPress={async () => {
+            await sendPushNotification(expoPushToken);
+          }}
+        />
         <AlarmsSection
           alarms={alarms}
           newAlarm={newAlarm}
