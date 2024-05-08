52b3b4d420aca4ff73564f4e10922082200e5668
lulemar16 <128646816+lulemar16@users.noreply.github.com>
2024-05-08 21:15:40 +0300

    home carrousel

--- my-app/screens/HomeScreen/CarrouselImages.js
+++ my-app/screens/HomeScreen/CarrouselImages.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, FlatList, Dimensions } from 'react-native';
+import banner1 from '../../assets/banner1.png';
+import banner2 from '../../assets/banner2.png';
+import banner3 from '../../assets/banner3.png';
 
 const images = [
-  { uri: 'https://esnaalto.org/sites/esnaalto.org/files/styles/spotlight/public/news/spotlight/DNA_emblem_pink_RGB_23102015.jpg?itok=46MOjFaA' },
-  { uri: 'https://ehandel.fi/wp-content/uploads/sites/2/2018/06/zalando.jpg' },
-  { uri: 'https://image.cnbcfm.com/api/v1/image/107228387-1681972090691-gettyimages-1247510608-raa-nokianew230226_npJYU.jpeg?v=1706171260&w=1920&h=1080' },
+  banner1,
+  banner2,
+  banner3,
 ];
 
 const { width } = Dimensions.get('window');
@@ -12,7 +15,7 @@ const { width } = Dimensions.get('window');
 const CarouselComponent = () => {
   const renderItem = ({ item }) => (
     <View style={styles.slide}>
-      <Image source={{ uri: item.uri }} style={styles.image} />
+      <Image source={item} style={styles.image} />
     </View>
   );
 
@@ -29,7 +32,7 @@ const CarouselComponent = () => {
       />
     </View>
   );
-};
+}; 
 
 const styles = StyleSheet.create({
   container: {
@@ -44,7 +47,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   image: {
-    width: '90%',
+    width: '99%',
     height: '100%',
     borderRadius: 10,
   },
